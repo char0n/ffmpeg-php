cd4fe73547da70ac4004cebdd200be9d5e23d343
char0n (Vladimir Gorej) <gorej@mortality.sk>
2010-03-26 21:13:49 +0100

    Production version 1.2

--- FFmpegAnimatedGif.php
+++ FFmpegAnimatedGif.php
@@ -11,7 +11,7 @@
 * @author char0n (Vladimir Gorej)
 * @package FFmpegPHP
 * @license New BSD 
-* @version 1.1
+* @version 1.2
 */
 class FFmpegAnimatedGif implements Serializable {
 
--- FFmpegFrame.php
+++ FFmpegFrame.php
@@ -5,7 +5,7 @@
 * @author char0n (Vladimir Gorej)
 * @package FFmpegPHP
 * @license New BSD
-* @version 1.1
+* @version 1.2
 */
 class FFmpegFrame implements Serializable {
     
