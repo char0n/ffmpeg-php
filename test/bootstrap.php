50d0d949bb0229d45308c6b8051d3f051c92f2b3
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-02-03 20:18:23 +0100

    test: add phpunit configuration file

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,17 +10,4 @@
  * @package FFmpegPHP  
  */
 
-date_default_timezone_set('Europe/Bratislava');  
-
-$basePath = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR;
-require_once $basePath.'FFmpegAutoloader.php';
-require_once $basePath.'provider'.DIRECTORY_SEPARATOR.'OutputProvider.php';
-require_once $basePath.'provider'.DIRECTORY_SEPARATOR.'AbstractOutputProvider.php';
-require_once $basePath.'provider'.DIRECTORY_SEPARATOR.'FFmpegOutputProvider.php';
-require_once $basePath.'provider'.DIRECTORY_SEPARATOR.'FFprobeOutputProvider.php';
-require_once $basePath.'FFmpegAnimatedGif.php';
-require_once $basePath.'FFmpegFrame.php';
-require_once $basePath.'FFmpegMovie.php';
-require_once $basePath.'adapter'.DIRECTORY_SEPARATOR.'ffmpeg_animated_gif.php';
-require_once $basePath.'adapter'.DIRECTORY_SEPARATOR.'ffmpeg_frame.php';
-require_once $basePath.'adapter'.DIRECTORY_SEPARATOR.'ffmpeg_movie.php';
+date_default_timezone_set('Europe/Bratislava');
