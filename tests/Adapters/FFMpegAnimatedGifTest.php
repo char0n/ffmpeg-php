5328fd8135ad77df15a392b7db945fa69bb7f2aa
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-08-23 13:23:13 +0200

    test(FFmpegAnimatedGifTest): fix tests for FFMpegAnimatedGif adapter

--- tests/Adapters/FFMpegAnimatedGifTest.php
+++ tests/Adapters/FFMpegAnimatedGifTest.php
@@ -85,7 +85,7 @@ class FFMpegAnimatedGifTest extends TestCase
         $saveResult = $this->anim->addFrame($this->frame1);
         $this->assertTrue($saveResult, 'Save result should be true');
         $this->assertTrue(file_exists(self::$outFilePath), 'File "'.self::$outFilePath.'" should exist after saving');
-        $this->assertEquals(30534, filesize(self::$outFilePath), 'Animation binary size should be int(30534)');
+        $this->assertEquals(31368, filesize(self::$outFilePath), 'Animation binary size should be int(31368)');
         $imageInfo = getimagesize(self::$outFilePath);
         $this->assertEquals(100, $imageInfo[0], 'Saved image width should be int(100)');
         $this->assertEquals(120, $imageInfo[1], 'Saved image height should be int(120)');
